6968f6b491c5440f9caa4862d14a42eb170e0486
Jesus Romero <jesucito_05@gmail.com>
2021-06-20 23:06:37 -0500

    Agregando commits anteriores

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent} from "./pages/home/home.component";
 import { StudentsComponent } from "./pages/students/students.component";
-import { StudentComponent } from "./pages/student/student.component";
 import { AboutComponent } from "./pages/about/about.component";
 import { PageNotFoundComponent } from "./pages/page-not-found/page-not-found.component";
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
-  { path: 'students', component: StudentsComponent },
-  { path: 'students/new', component: StudentComponent },
-  { path: 'students/:id', component: StudentComponent },
+  { path: 'memberships', component: StudentsComponent },
   { path: 'about', component: AboutComponent },
   { path: '**', component: PageNotFoundComponent }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StudentsComponent } from './pages/students/students.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
-import { StudentComponent } from './pages/student/student.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatGridListModule } from "@angular/material/grid-list";
@@ -20,7 +19,6 @@ import { FormsModule } from "@angular/forms";
 import { MatTableModule } from "@angular/material/table";
 import { MatPaginatorModule } from "@angular/material/paginator";
 import { HttpClientModule } from "@angular/common/http";
-import {StudentsApiService} from "./services/students-api.service";
 import {MatTooltipModule} from "@angular/material/tooltip";
 
 @NgModule({
@@ -29,7 +27,6 @@ import {MatTooltipModule} from "@angular/material/tooltip";
     StudentsComponent,
     HomeComponent,
     AboutComponent,
-    StudentComponent,
     PageNotFoundComponent
   ],
     imports: [
@@ -49,7 +46,7 @@ import {MatTooltipModule} from "@angular/material/tooltip";
         HttpClientModule,
         MatTooltipModule
     ],
-  providers: [StudentsApiService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
