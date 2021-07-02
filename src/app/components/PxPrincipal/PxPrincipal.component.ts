c936ea48b3a35c0645d15c6d53021f3b9486373e
Dana Brigitte Vallejos Nestares <dana_s94@hotmail.com>
2021-07-01 21:08:27 -0500

    Agregando PxPrincipal

---
+++ src/app/components/PxPrincipal/PxPrincipal.component.ts
@@ -0,0 +1,10 @@
+import {Component, EventEmitter, Input, Output} from "@angular/core";
+
+@Component({
+  selector: 'px-principal',
+  templateUrl: './PxPrincipal.component.html',
+  styleUrls: ['./PxPrincipal.component.css']
+})
+export class PxPrincipalComponent{
+  buttonColor: "white"
+}
